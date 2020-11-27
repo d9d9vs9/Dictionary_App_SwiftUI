ca1634c8720d19d8cf6f8f3eb389c3392d5039a0
Admin <admin@Mac-Admin.local>
2020-11-27 20:57:03 +0300

    added CoreDataStack and folders

--- MyDictionary/MyDictionary/MyDictionary/Resources/SceneDelegate/SceneDelegate.swift
+++ MyDictionary/MyDictionary/MyDictionary/Resources/SceneDelegate/SceneDelegate.swift
@@ -19,7 +19,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
 
         // Get the managed object context from the shared persistent container.
-        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        let context = CoreDataStack.shared.viewContext
 
         // Create the SwiftUI view and set the context as the value for the managedObjectContext environment keyPath.
         // Add `@Environment(\.managedObjectContext)` in the views that will need the context.
@@ -62,9 +62,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // to restore the scene back to its current state.
 
         // Save changes in the application's managed object context when the application transitions to the background.
-        (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
+        CoreDataStack.shared.saveContext()
     }
 
-
 }
-
