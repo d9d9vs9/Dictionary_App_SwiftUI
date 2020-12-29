e3173c3591b9748870e5e3b4999330af1a75d898
Dmitry Chumakov <dima.chumakovwork@gmail.com>
2020-12-29 21:56:22 +0200

    added Endpoint

---
+++ MyDictionary/MyDictionary/MyDictionary/Services/Network/Endpoint/Endpoint.swift
@@ -0,0 +1,15 @@
+//
+//  Endpoint.swift
+//  MyDictionary
+//
+//  Created by Дмитрий Чумаков on 29.12.2020.
+//
+
+import Foundation
+
+protocol Endpoint {
+    var path: String { get }
+    var httpMethod: HTTPMethod { get }
+    var httpHeaders: HTTPHeader { get }
+    var httpParameters: HTTPParameters? { get }
+}
