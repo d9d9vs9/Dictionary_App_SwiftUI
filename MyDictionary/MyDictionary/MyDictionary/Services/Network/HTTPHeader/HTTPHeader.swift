fa368d82d1954b6452447182d66cce6c5ec83d78
Dmitry Chumakov <dima.chumakovwork@gmail.com>
2020-12-29 18:43:50 +0200

    added http method and header

---
+++ MyDictionary/MyDictionary/MyDictionary/Services/Network/HTTPHeader/HTTPHeader.swift
@@ -0,0 +1,10 @@
+//
+//  HTTPHeader.swift
+//  MyDictionary
+//
+//  Created by Дмитрий Чумаков on 29.12.2020.
+//
+
+import Foundation
+
+typealias HTTPHeader = [String : String]
---
+++ MyDictionary/MyDictionary/MyDictionary/Services/Network/HTTPMethod/HTTPMethod.swift
@@ -0,0 +1,15 @@
+//
+//  HTTPMethod.swift
+//  MyDictionary
+//
+//  Created by Дмитрий Чумаков on 29.12.2020.
+//
+
+import Foundation
+
+enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case delete = "DELETE"
+}
