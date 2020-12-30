887aa20be47c3bd16d5547728c4d4909b7226f04
Dmitry Chumakov <dima.chumakovwork@gmail.com>
2020-12-30 13:28:57 +0200

    added HTTPHeaderConstants

--- MyDictionary/MyDictionary/MyDictionary/Utilites/Constants/Constants.swift
+++ MyDictionary/MyDictionary/MyDictionary/Utilites/Constants/Constants.swift
@@ -19,5 +19,18 @@ struct Constants {
     struct CoreData {
         static let fetchLimit: Int = 10
     }
+    
+    struct HTTPHeaderConstants {
+        
+        static let contentType: String = "Content-Type"
+        static let applicationJson: String = "application/json"
+        
+        /// return
+        /// Content-Type : application/json        
+        static func defaultHeaders() -> HTTPHeader {
+            return [contentType : applicationJson]
+        }
+        
+    }
 
 }
