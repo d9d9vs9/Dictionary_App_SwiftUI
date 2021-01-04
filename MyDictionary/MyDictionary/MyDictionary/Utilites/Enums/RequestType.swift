8c3ac260cbe4cc6b4fb4ecc102a066452e0e10fa
Dmitry Chumakov <dima.chumakovwork@gmail.com>
2021-01-04 15:44:28 +0200

    edited RequestType

--- MyDictionary/MyDictionary/MyDictionary/Utilites/Enums/RequestType.swift
+++ MyDictionary/MyDictionary/MyDictionary/Utilites/Enums/RequestType.swift
@@ -10,8 +10,4 @@ import Foundation
 enum RequestType {
     /// Will translate to a URLSessionDataTask.
     case data
-    /// Will translate to a URLSessionDownloadTask.
-    case download
-    /// Will translate to a URLSessionUploadTask.
-    case upload
 }
